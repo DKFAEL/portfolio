d5eb0a86b7dc5bcc22fe6c3ef319d7414cb88bf3
DKFAEL <rafaelnunesemp@hotmail.com>
2024-01-29 23:57:21 -0300

    Finalizando section projeto

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { ProjetosComponent } from './projetos/projetos.component';
+
 import { HabilidadeCardsComponent } from './habilidade-cards/habilidade-cards.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { MainSkillComponent } from './main-skill/main-skill.component';
+import { ProjectComponent } from './projects/project.component';
 
 
 
@@ -12,9 +13,10 @@ import { MainSkillComponent } from './main-skill/main-skill.component';
 @NgModule({
   declarations: [
     HeaderComponent,
-    ProjetosComponent,
     HabilidadeCardsComponent,
     MainSkillComponent,
+    ProjectComponent
+    
     
   ],
   imports: [
@@ -25,7 +27,8 @@ import { MainSkillComponent } from './main-skill/main-skill.component';
   exports: [
     HeaderComponent,
    HabilidadeCardsComponent,
-   MainSkillComponent
+   MainSkillComponent,
+   ProjectComponent
   ]
 })
 export class ComponentsModule { }
